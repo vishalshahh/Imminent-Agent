9989e549215b3fb0e47494df47d3b937f2fe1ac9
Vishal <Vishal12kl@gmail.com>
2023-12-05 22:00:34 +0530

    bug fix

--- components/minibot/ChatInput.tsx
+++ components/minibot/ChatInput.tsx
@@ -24,7 +24,7 @@ const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
     setIsMessageUpdating,
   } = useContext(MessagesContext)
 
-  const { mutate: sendMessage, isPending } = useMutation({
+  const { mutate: sendMessage, isLoading } = useMutation({
     mutationKey: ['sendMessage'],
     // include message to later use it in onMutate
     mutationFn: async (_message: Message) => {
@@ -105,7 +105,7 @@ const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
           maxRows={4}
           value={input}
           autoFocus
-          disabled={isPending}
+          disabled={isLoading}
           onChange={(e) => setInput(e.target.value)}
           placeholder='Write a message...'
           className='peer disabled:opacity-50 pr-14 resize-none block w-full border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6'
@@ -113,7 +113,7 @@ const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
 
         <div className='absolute inset-y-0 right-0 flex py-1.5 pr-1.5'>
           <kbd className='inline-flex items-center rounded border bg-white border-gray-200 px-1 font-sans text-xs text-gray-400'>
-            {isPending ? (
+            {isLoading ? (
               <Loader2 className='w-3 h-3 animate-spin' />
             ) : (
               <CornerDownLeft className='w-3 h-3' />
